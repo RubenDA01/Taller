05b2e273add03306036e09c0a9910a4575346034
Ruben <rduaave296aieslacampina.es>
2024-11-17 13:57:42 +0100

    base de datos

---
+++ taller.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-11-2024 a las 13:56:15
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `taller`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(3) NOT NULL,
+  `Nombre_cliente` varchar(50) NOT NULL,
+  `Telefono` int(50) NOT NULL,
+  `Correo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `Nombre_cliente`, `Telefono`, `Correo`) VALUES
+(11, 'Ruben', 684213022, 'RUBEN23@RUBEN.ES'),
+(27, 'BARTOLO', 678987789, 'BARTOLITO@BAR.BAR'),
+(28, 'makiman', 123456789, 'MAKIMAN@MAKIMAN.ES'),
+(29, 'Alex el LION', 789000666, 'alesXIIO@eee.eee'),
+(30, 'GARCIA', 2147483647, '123123123123@WE.E');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `coches`
+--
+
+CREATE TABLE `coches` (
+  `id_coche` int(3) NOT NULL,
+  `id_cliente` int(3) NOT NULL,
+  `Marca` varchar(50) NOT NULL,
+  `Modelo` varchar(50) NOT NULL,
+  `Año` int(50) NOT NULL,
+  `Matricula` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `coches`
+--
+
+INSERT INTO `coches` (`id_coche`, `id_cliente`, `Marca`, `Modelo`, `Año`, `Matricula`) VALUES
+(8, 27, 'RENAULT', 'MEGANE', 2011, '1111KTM'),
+(9, 11, 'SUBARU', 'IMPREZA', 2001, '3017CKM'),
+(19, 29, 'FERRARI', 'F15', 2024, '1010MMM'),
+(22, 29, 'TUKTUK', 'TUCTUC', 2021, '2324ERER'),
+(24, 11, 'BMW M', 'X8', 2019, '8567JVS'),
+(25, 30, 'AUDI', 'A3', 2007, '1234POL'),
+(26, 27, 'FIAT', 'MULTIPLA', 2003, '8998AAA');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `id_servicio` int(3) NOT NULL,
+  `id_coche` int(3) NOT NULL,
+  `Tipo_servicio` varchar(50) NOT NULL,
+  `Precio` int(50) NOT NULL,
+  `Fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `servicios`
+--
+
+INSERT INTO `servicios` (`id_servicio`, `id_coche`, `Tipo_servicio`, `Precio`, `Fecha`) VALUES
+(1, 24, 'Cambio de aceite', 100, '2024-11-12'),
+(2, 24, 'Pinza de Frenos', 40, '2024-11-08'),
+(4, 22, 'Techo', 80, '2024-11-06');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `coches`
+--
+ALTER TABLE `coches`
+  ADD PRIMARY KEY (`id_coche`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indices de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`id_servicio`),
+  ADD KEY `id_coche` (`id_coche`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT de la tabla `coches`
+--
+ALTER TABLE `coches`
+  MODIFY `id_coche` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `id_servicio` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `coches`
+--
+ALTER TABLE `coches`
+  ADD CONSTRAINT `coches_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD CONSTRAINT `servicios_ibfk_1` FOREIGN KEY (`id_coche`) REFERENCES `coches` (`id_coche`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
